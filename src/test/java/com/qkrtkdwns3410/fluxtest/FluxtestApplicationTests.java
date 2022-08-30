96ece5599cf69e4934b3ce1c2ff32e5ef5e4958e
jihye94 <qkrtkdwns3410@naver.com>
2022-08-30 15:16:14 +0900

    웹 플럭스 공부용

---
+++ src/test/java/com/qkrtkdwns3410/fluxtest/FluxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qkrtkdwns3410.fluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxtestApplicationTests {
+      
+      @Test
+      void contextLoads() {
+      }
+      
+}
