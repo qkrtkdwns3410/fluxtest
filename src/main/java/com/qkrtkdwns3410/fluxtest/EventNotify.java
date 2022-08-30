c00d7de0ec41d6e0b24f42a4189a82c0e0278e66
jihye94 <qkrtkdwns3410@naver.com>
2022-08-30 16:57:22 +0900

    feat: 노가다 코드 완성 + reactive streams 라이브러리로 코드 수정 예정
    Author: 박상준

--- src/main/java/com/qkrtkdwns3410/fluxtest/EventNotify.java
+++ src/main/java/com/qkrtkdwns3410/fluxtest/EventNotify.java
@@ -1,5 +1,7 @@
 package com.qkrtkdwns3410.fluxtest;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -17,6 +19,8 @@ import java.util.List;
  * 2022-08-30        qkrtkdwns3410       최초 생성
  */
 @Component
+@Setter
+@Getter
 public class EventNotify {
       
       private List<String> events = new ArrayList<>();
@@ -24,17 +28,13 @@ public class EventNotify {
       private boolean change = false;
       
       public void add(String data) {
-      
+            
             events.add(data);
             change = true; // 데이터가 들어오는 순간 true로 변경된다.
       }
       
       public boolean getChanges() {
             return change;
-      }
-      
-      public List<String> getEvents() {
-            return events;
       }
       
 }
--- src/main/java/com/qkrtkdwns3410/fluxtest/MyFilter.java
+++ src/main/java/com/qkrtkdwns3410/fluxtest/MyFilter.java
@@ -16,6 +16,7 @@ import java.io.PrintWriter;
  * -----------------------------------------------------------
  * 2022-08-30        qkrtkdwns3410       최초 생성
  */
+// 진짜 노가다 코드 방안
 public class MyFilter implements Filter {
       private EventNotify eventNotify;
       
@@ -32,6 +33,7 @@ public class MyFilter implements Filter {
             // 하지만 text/event-stream의 경우 플러시 할때 마다 읽어서 화면에 뿌리게 된다
             PrintWriter out = servletResponse.getWriter();
             
+            // 1. Reactive Streams 라이브러리를 사용하면 표준을 지켜서 응답을 할 수 있음
             for (int i = 0; i < 5; i++) {
                   out.println("응답" + i); // 버퍼에 데이터 쌓고
                   out.flush(); // 버퍼를 비우는 역할
@@ -45,14 +47,24 @@ public class MyFilter implements Filter {
             }
             //원래는 종료됨.
             // 스택이 종료 되지 않도록.. ->  flux 의 개념
+            // 2. SSE Emitter 라이브러리 사용시 편하게 사용가능합니다. 
             while (true) {
                   try {
+                        if (eventNotify.getChanges()) {
+                              int lastIndex = eventNotify.getEvents()
+                                                         .size() - 1;
+                              out.println("응답: " + eventNotify.getEvents()
+                                                              .get(lastIndex));
+                              out.flush();
+                              eventNotify.setChange(false);
+                        }
                         Thread.sleep(1);
                   } catch (InterruptedException e) {
                         e.printStackTrace();
                   }
             }
-            //여기서 sse 적용해보겠습니다.
             
+            // 3. WebFlux - >  Reactive Streams 이 친구가 적용된 stream을 배우고 (비동기 단일스레드동작) -> 효과적...
+            // 4. Servlet MVC - >  Reactive Streams 이 친구가 적용된 stream을 배우고 (멀티 스레드 방식)
       }
 }
